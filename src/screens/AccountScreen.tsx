91c1ebfd43a86cb14f2359a9ddf52ceebcb0467e
Denis Brester <49287397+DenKick@users.noreply.github.com>
2022-05-16 01:01:42 +0300

    Update screens (Account and Auth)

--- src/screens/AccountScreen.tsx
+++ src/screens/AccountScreen.tsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
+import styled from '@emotion/native'
+
+import Logo from '~components/Logo'
+import ProfileMenuItem from '~components/ProfileMenuItem'
 import ScreenLayout from '~components/ScreenLayout'
+import MenuItem from '~icons/MenuItem'
+import { useAppDispatch } from '~store/index'
+import { fetchUserInfo } from '~store/profileSlice'
+import { RoutesNames } from '~types/routes'
+
+const Wrapper = styled.ScrollView`
+  height: 100%;
+  padding: ${({ theme }) => theme.dimensions.commonHorizontalPadding};
+`
+
+const LogoWrapper = styled.View`
+  padding: 30px 0 40px;
+`
 
 const AccountScreen: React.FC = () => {
-  return <ScreenLayout heading={'Профиль'} offset={0} />
+  const dispatch = useAppDispatch()
+
+  useEffect(() => {
+    dispatch(fetchUserInfo())
+  }, [])
+
+  return (
+    <ScreenLayout heading={'Профиль'} offset={0}>
+      <Wrapper contentContainerStyle={{ alignItems: 'center' }}>
+        <LogoWrapper>
+          <Logo scale={0.3} />
+        </LogoWrapper>
+        <ProfileMenuItem />
+        <MenuItem title='Новости' route={RoutesNames.Timetable} />
+        <MenuItem title='Уведомления' route={RoutesNames.Timetable} />
+        <MenuItem title='Сообщения' route={RoutesNames.Timetable} />
+      </Wrapper>
+    </ScreenLayout>
+  )
 }
 
 export default AccountScreen
