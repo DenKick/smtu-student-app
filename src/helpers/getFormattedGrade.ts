326cd30971da8fe0b1e5eb54e3a90d4b6d2872cb
Denis Brester <49287397+DenKick@users.noreply.github.com>
2022-05-23 00:27:25 +0300

    Fix getFormattedGrade helper

--- src/helpers/getFormattedGrade.ts
+++ src/helpers/getFormattedGrade.ts
@@ -1,9 +1,10 @@
-import { GradeTypes, Performance } from '~types/performance'
+import { ExamTypes, GradeTypes, Performance } from '~types/performance'
 
 const getFormattedGrade = (item: Performance) => {
   if (item.exam) {
     return {
       type: item.exam > 2 ? GradeTypes.Positive : GradeTypes.Negative,
+      exam: ExamTypes.Exam,
       grade: item.exam.toString(),
     }
   }
@@ -11,6 +12,7 @@ const getFormattedGrade = (item: Performance) => {
   if (item.differentialCredit) {
     return {
       type: item.differentialCredit > 2 ? GradeTypes.Positive : GradeTypes.Negative,
+      exam: ExamTypes.DiffCredit,
       grade: item.differentialCredit.toString(),
     }
   }
@@ -18,6 +20,7 @@ const getFormattedGrade = (item: Performance) => {
   if (item.credit === false) {
     return {
       type: GradeTypes.Negative,
+      exam: ExamTypes.Credit,
       grade: 'Незачет',
     }
   }
@@ -25,12 +28,14 @@ const getFormattedGrade = (item: Performance) => {
   if (item.credit === true) {
     return {
       type: GradeTypes.Positive,
+      exam: ExamTypes.Credit,
       grade: 'Зачет',
     }
   }
 
   return {
     type: GradeTypes.Null,
+    exam: GradeTypes.Null,
     grade: '',
   }
 }
